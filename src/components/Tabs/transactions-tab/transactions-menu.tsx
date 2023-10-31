31bc2b1055e49a69348c43338a2c6b4099185f20
Yury Gagarin <yury_gagarin2017@mail.ru>
2023-10-31 16:21:06 +0000

    - added Transaction menu - Add transactions button

---
+++ src/components/Tabs/transactions-tab/transactions-menu.tsx
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+
+function TransactionsMenu() {
+  return (
+    <Button
+    //   style={{ marginTop: 5, marginBottom: 5, textAlign: "left" }}
+    //   type="primary"
+    >
+      Add
+    </Button>
+  );
+}
+
+export default TransactionsMenu;
--- src/components/Tabs/transactions-tab/transactions-tab.tsx
+++ src/components/Tabs/transactions-tab/transactions-tab.tsx
@@ -3,6 +3,8 @@ import { useBankAccountsContext } from "../../../context/bank-accounts-context";
 import { useTransactionsContext } from "../../../context/transactions-context";
 import TransactionsTable from "./transactions-table";
 import TheSkeleton from "../../../UI/the-skeleton";
+import TransactionsMenu from "./transactions-menu";
+import { Col, Row, Space } from "antd";
 
 function TransactionsTab() {
   const { selectedBankAccountId } = useBankAccountsContext();
@@ -14,7 +16,22 @@ function TransactionsTab() {
   }, [selectedBankAccountId]);
 
   return (
-    <TransactionsTable transactions={transactions} isLoading={isLoading} />
+    <>
+      <Row>
+        <Col
+          span={24}
+          style={{ display: "flex", marginTop: 10, marginBottom: 10 }}
+        >
+          <TransactionsMenu />
+        </Col>
+        <Col span={24}>
+          <TransactionsTable
+            transactions={transactions}
+            isLoading={isLoading}
+          />
+        </Col>
+      </Row>
+    </>
   );
 }
 
